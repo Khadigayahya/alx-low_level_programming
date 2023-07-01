158d49b2f75964d40f4ef623d5281e029a8b43c3
khadigayahya <khadigayahya84@gmail.com>
2023-06-30 19:17:13 -0700

    commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,19 +2,19 @@
 #include "lists.h"
 
 /**
- * list_len - determines length of linked list
- * @h: pointer to first node
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
  *
- * Return: size of list
+ * Return: number of elements in h
  */
-size_t list_len(const list_t *h);
+size_t list_len(const list_t *h)
 {
-	size_t i = 0;
+	size_t n = 0;
 
 	while (h)
 	{
-		i++;
+		n++;
 		h = h->next;
 	}
-	return (i);
+	return (n);
 }
